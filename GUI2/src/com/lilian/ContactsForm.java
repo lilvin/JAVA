a4a97de42f642d0d0a63052ea65cef1635abf20f
lilian <liliankirito@gmail.com>
2018-08-02 10:58:43 +0300

    another GUI form

---
+++ GUI2/src/com/lilian/ContactsForm.java
@@ -0,0 +1,25 @@
+package com.lilian;
+
+import javax.swing.*;
+
+public class ContactsForm {
+    private JPanel ContactPanel;
+    private JPanel TextfieldPanel;
+    private JPanel LabelPanel;
+    private JPanel ButtonPanel;
+    private JLabel givenNamesLabel;
+    private JLabel SurnameLabel;
+    private JLabel MobileLabel;
+    private JLabel EmailLabel;
+    private JTextField GivenNameTf;
+    private JTextField EmailTf;
+    private JTextField SurnameTf;
+    private JTextField MobileTf;
+    private JButton AddBtn;
+    private JButton noOpBtn;
+    private JButton NewContactBtn;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
